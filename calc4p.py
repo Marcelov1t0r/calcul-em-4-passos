43724a9099b4101df1a7b6f989a5be979f4d69e3
Aluno Lab 135 <lab135@lab.senai>
2023-08-25 20:11:05 -0300

    Calcula soma e subtração

--- calc4p.py
+++ calc4p.py
@@ -1,3 +1,9 @@
 x=int(input("Digite um número: "))
 y=int(input("Digite outro número: "))
-print("O resultado é:",x+y)
+
+opcao = int(input("Escolha uma opção: "))
+
+if opcao == 1:
+    print("O resultado é:",x+y)
+elif opcao == 2:
+    print("O resultado é:",x-y)
